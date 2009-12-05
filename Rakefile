a3f96ce4d1aab01c27d6ac730d357de4c8bd6d6a
Alex MacCaw <maccman@gmail.com>
2009-12-05 22:41:19 +0000

    first commit

---
+++ Rakefile
@@ -0,0 +1,14 @@
+task :default => :build
+task :build do
+  opts  = `wx-config --libs`.chomp
+  opts  += `wx-config --cxxflags`.chomp
+  opts  += ' -I/usr/local/lib/ruby19/1.9.1/i386-darwin9.7.0'
+  opts  += ' -I/usr/local/include/ruby19-1.9.1'
+  opts  += ' -I/usr/local/include/ruby19-1.9.1/i386-darwin9.7.0'
+  opts  += ' -lruby19'
+  `g++ main.cpp #{opts} -o main`
+end
+
+task :run do
+  exec './main'
+end
---
+++ main.cpp
@@ -0,0 +1,90 @@
+#include <wx/wx.h>
+#include <ruby.h>
+
+class App : public wxApp
+{
+    virtual bool OnInit();
+    virtual void InitRuby();
+};
+
+IMPLEMENT_APP(App)
+
+class MainFrame : public wxFrame
+{
+public:
+    MainFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+    void OnQuit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+};
+
+enum
+{
+    ID_Quit=1,
+    ID_About
+};
+
+
+bool App::OnInit()
+{
+    // Show splash screen
+    // http://docs.wxwidgets.org/stable/wx_wxsplashscreen.html#wxsplashscreen
+    MainFrame *frame = new MainFrame( _("Bowline"), wxPoint(50, 50),
+                                  wxSize(450, 350));
+    
+    // Add webkit instance to frame
+    // Load ruby
+    // Expose some webkit methods to Ruby
+    // Load init.rb
+    // Unlock Ruby (so threads can run)
+
+    frame->Show(true);
+    SetTopWindow(frame);
+    
+    return true;
+}
+
+void App::InitRuby(){
+    RUBY_INIT_STACK;
+    ruby_init();
+    ruby_script("Embedded Ruby");
+    ruby_init_loadpath();
+
+    // ruby_incpush("app_path");
+  
+    // Since ruby_init_gems is not public
+    // rb_define_module("Gem");
+    // Init_prelude();
+}
+
+MainFrame::MainFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
+    : wxFrame( NULL, -1, title, pos, size )
+{
+    wxMenuBar *menuBar = new wxMenuBar;
+
+    wxMenu *menuFile = new wxMenu;
+
+    menuFile->Append( ID_About, _("&About...") );
+    menuFile->AppendSeparator();
+    menuFile->Append( ID_Quit, _("E&xit") );
+
+    menuBar->Append(menuFile, _("&File") );
+
+    SetMenuBar(menuBar);
+    
+    this->Connect( ID_Quit, wxEVT_COMMAND_MENU_SELECTED,
+                    (wxObjectEventFunction) &MainFrame::OnQuit );
+    this->Connect( ID_About, wxEVT_COMMAND_MENU_SELECTED,
+                    (wxObjectEventFunction) &MainFrame::OnAbout );
+}
+
+void MainFrame::OnQuit(wxCommandEvent& WXUNUSED(event))
+{
+    Close(true);
+}
+
+void MainFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
+{
+    wxMessageBox( _("wxWidgets Hello World example."), 
+                  _("About Hello World"),
+                  wxOK|wxICON_INFORMATION, this );
+}
