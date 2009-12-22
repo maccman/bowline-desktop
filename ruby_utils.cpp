c7a4d04f086487cb1559e2a8cbe138d1ad73d9ce
Alex MacCaw <maccman@gmail.com>
2009-12-22 19:40:50 +0000

    Show error backtrace

--- ruby_utils.cpp
+++ ruby_utils.cpp
@@ -6,6 +6,9 @@ public:
   static void LogError(){
     VALUE lasterr = rb_gv_get("$!");
     VALUE message = rb_obj_as_string(lasterr);
-    std::cout << RSTRING_PTR(message) << std::endl;
+    VALUE array   = rb_funcall(lasterr, rb_intern("backtrace"), 0);
+    VALUE backtrace = rb_funcall(array, rb_intern("join"), 1, rb_str_new2("\n\t"));
+    std::cout << "\t" << RSTRING_PTR(message) << std::endl;
+    std::cout << RSTRING_PTR(backtrace) << std::endl;
   }
 };
