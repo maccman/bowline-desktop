1d027775d82df2782edb89bc0af2fb5c4eb98af4
Alex MacCaw <maccman@gmail.com>
2010-02-14 00:49:11 +0000

    gtk

--- bowline/bowline_webkit_gtk.cpp
+++ bowline/bowline_webkit_gtk.cpp
@@ -6,22 +6,38 @@ bool BowlineWebKit::Create(wxWindow *parent,
                            const wxSize& size, long style,
                            const wxValidator& validator,
                            const wxString& name){
-  m_webView = webkit_web_view_new();
+                             
+  if (!PreCreation( parent, pos, size ) ||
+    !CreateBase( parent, winID, pos, size, style, validator, name ))
+  {
+    wxFAIL_MSG( wxT("BowlineWebKit creation failed") );
+    return false;
+  }
   
-  WebKitWebSettings* settings = webkit_web_settings_new();
-	g_object_set(G_OBJECT(settings), 
-		"enable-developer-extras", TRUE,
-		"enable-universal-access-from-file-uris", TRUE,
-		"javascript-can-open-windows-automatically", FALSE,
-		NULL);
-	webkit_web_view_set_settings(WEBKIT_WEB_VIEW(m_webView), settings);
-	
-	const char* cUserAgent = 0;
-	g_object_get(G_OBJECT(settings), "user-agent", &cUserAgent, NULL);
-	userAgent.append(cUserAgent);
-	userAgent.append(" Bowline");
-	g_object_set(G_OBJECT(settings), "user-agent", userAgent.c_str(), NULL);
-	// TODO
+  m_webView = webkit_web_view_new();
+  g_object_ref(m_webView);
+  
+  //   WebKitWebSettings* settings = webkit_web_settings_new();
+  // g_object_set(G_OBJECT(settings), 
+  //  "enable-developer-extras", TRUE,
+  //  "enable-universal-access-from-file-uris", TRUE,
+  //  "javascript-can-open-windows-automatically", FALSE,
+  //  NULL);
+  // webkit_web_view_set_settings(WEBKIT_WEB_VIEW(m_webView), settings);
+  // 
+  // const char* cUserAgent = 0;
+  // g_object_get(G_OBJECT(settings), "user-agent", &cUserAgent, NULL);
+  // userAgent.append(cUserAgent);
+  // userAgent.append(" Bowline");
+  // g_object_set(G_OBJECT(settings), "user-agent", userAgent.c_str(), NULL);
+  
+  
+  m_parent->DoAddChild( this );
+
+  PostCreation(size);
+  
+  // gtk_widget_show(m_webView);
+  // gtk_container_add (GTK_CONTAINER (scrolled_window), web_view);
 }
 
 BowlineWebKit::~BowlineWebKit() { 
